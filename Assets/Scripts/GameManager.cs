f108c042cfb1e95ee6ad7cd03ec6c694b0136a92
carloslajarasanchez <carloslajarasanchez@gmail.com>
2025-11-25 19:43:49 +0100

    Añadida escena y gameManager, Materiales

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsMasterClient)// si soy el master de la sala
+        {
+            PhotonNetwork.Instantiate("PlayerA", new Vector3(), Quaternion.identity);// Instanciamos un jugador master
+        }
+        else
+        {
+            PhotonNetwork.Instantiate("PlayerB", new Vector3(), Quaternion.identity);// Instanciamos el jugador
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
